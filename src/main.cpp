5fc9503fd8f46b7730e6ce52ec947997cb0a5b9b
Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>
2023-06-07 13:09:37 +0200

    Added thread, chrono and random to randomly generate values of DD.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,38 @@
 #include "../include/include.h"
+#include <random>
+#include <chrono>
+#include <thread>
+
+double rng01() {
+    return drand48();
+}
 
 
 int main (int argc, char** argv) {
     
     //std::cout << Coherent::A_integrand_function(1.20915, 0.199206, 2.82996, 1.30603,0.3,0.5,0.0,1) << std::endl;
-    std::cout << 9.0*SaturationModel::DD(0.6674, -2.53162, -7.99407, 0.312111, -1.90802, 7.947, -0.793541, -5.98289) << std::endl;
-    std::cout << SaturationModel::DDEigen(0.6674, -2.53162, -7.99407, 0.312111, -1.90802, 7.947, -0.793541, -5.98289) << std::endl;
+    std::cout << 9.0*SaturationModel::DD(06.674, -25.3162, -79.9407, 03.12111, -19.0802, 79.47, -07.93541, -59.8289) << std::endl;
+    std::cout << SaturationModel::DDEigen(06.674, -25.3162, -79.9407, 03.12111, -19.0802, 79.47, -07.93541, -59.8289) << std::endl;
+
+    double counter = 0.0;
+    while (true) {
+        double bx = (5+counter)*2*(rng01()-0.5);
+        double by = (5+counter)*2*(rng01()-0.5);
+        double bbx = (5+counter)*2*(rng01()-0.5);
+        double bby = (5+counter)*2*(rng01()-0.5);
+        double rx = (5+counter)*2*(rng01()-0.5);
+        double ry = (5+counter)*2*(rng01()-0.5);
+        double rbx = (5+counter)*2*(rng01()-0.5);
+        double rby = (5+counter)*2*(rng01()-0.5);
+
+        double printthis = 9.0*SaturationModel::DDEigen(bx,by,bbx,bby,rx,ry,rbx,rby);
+
+        //std::cout << bvalue << "\t" << bbvalue << "\t" << _a << "\t" << _b << "\t" << _c << "\t" << _d << "\t" << GBWModel::dsigma_dip_d2b(bx,by,rx,ry) << std::endl;
+        std::cout << printthis << "\t" << bx << ", " << by << ", " << bbx << ", " << bby << "," << rx << ", " << ry << ", " << rbx << ", " << rby << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(150));
+        counter += 0.1;
+    }
 /*
     CubaConfig c_config;
     IntegrandParams i_params;
