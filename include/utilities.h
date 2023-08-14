6ddb0e4f820e734c6b12e3a380ca91f4b1f9d8b0
Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>
2023-08-14 23:03:02 +0200

    Added function for automatic reading in of data set for specific m.

--- include/utilities.h
+++ include/utilities.h
@@ -2,22 +2,19 @@
 
 
 #include <gsl/gsl_sf.h>
+#include <string>
 
 
 typedef long unsigned int luint;
 
-inline double get_b_range_factor() {
-    return 15.0;
-}
 
-inline double get_r_range_factor() {
-    return 20.0;
-}
-
-
-inline double sqr (double x) {
+inline double sqr (double x)
+{
     return x*x;
 }
 
 
 double bessel_K_safe (int n, double x);
+
+
+void set_import_filepath_by_m (std::string& filepath);
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,9 +1,68 @@
 #include "../include/utilities.h"
 
 #include <gsl/gsl_sf.h>
-
+#include <iostream>
+#include "../include/constants.h"
+#include <fstream>
+#include "../Interpolation3D/include/Interpolator3D.h"
+#include "../include/GBWModel.h"
+#include "../include/IntegrationRoutines.h"
 
 double bessel_K_safe (int n, double x) {
     if (x==0) {x = 1.0e-20;}
     return gsl_sf_bessel_Kn(n,x);
+}
+
+
+void set_import_filepath_by_m (std::string& filepath)
+{
+    std::string m_path;
+    m_path = std::to_string(int(10*m));
+
+    if (m_path.size()!=2)
+    {
+        std::cerr << "m_path is not length 2! Aborting" << std::endl;
+        exit(0);
+    }
+
+    filepath = filepath+"_m_"+m_path+".dat";
+
+    std::ifstream file_check (filepath);
+
+    if (!file_check)
+    {
+        file_check.close();
+        std::string answer;
+
+        std::cout << "No data for m=" << m << ". Do you want to generate that data set? (y/n)\nThe path to the new file would be " << filepath << "." << std::endl;
+        bool input_accepted = false;
+        while (!input_accepted)
+        {
+            std::cin >> answer;
+            if (answer=="y")
+            {
+                input_accepted = true;
+
+                std::cout << "Generating new data set..." << std::endl;
+                DataGenerationConfig config;
+                GBWModel::G_ip.generate_data(GBWModel::G_wrapper,&config,true);
+                GBWModel::G_ip.export_data(filepath);
+            }
+            else if (answer=="n")
+            {
+                input_accepted = true;
+
+                std::cout << "Aborting" << std::endl;
+                exit(0);
+            }
+            else
+            {
+                std::cout << "Please enter either \"y\" or \"n\"." << std::endl;
+            }
+        }
+    }
+    else 
+    {
+        file_check.close();
+    }
 }
