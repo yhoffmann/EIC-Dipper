143a52e43a017bfbc7549a94e9e928dc8f9c67a2
Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>
2023-09-14 22:35:16 +0200

    Updated include paths to match submodule Interpolation3D.

--- include/GBWModel.hpp
+++ include/GBWModel.hpp
@@ -2,29 +2,29 @@
 
 
 #include <cuba.h>
-#include "../Interpolation3D/include/Interpolator3D.h"
+#include "../Interpolation3D/include/Interpolator3D.hpp"
 
 
 namespace GBWModel {
     extern Interpolator3D G_ip;
 
-    double T_times_sigma0 (double b1, double b2);
+    double T_times_sigma0(double b1, double b2);
 
-    double Q_s_sqr (double b1, double b2);
+    double Q_s_sqr(double b1, double b2);
 
-    double G_old (double x1, double x2, double y1, double y2);
+    double G_old(double x1, double x2, double y1, double y2);
 
-    double Kmod (double x1, double x2, double y1, double y2);
+    double Kmod(double x1, double x2, double y1, double y2);
 
-    double G_mod (double x1, double x2, double y1, double y2);
+    double G_mod(double x1, double x2, double y1, double y2);
 
-    double G_integrand_function (double u, double v, double x1, double x2, double y1, double y2);
+    double G_integrand_function(double u, double v, double x1, double x2, double y1, double y2);
 
-    int G_integrand (const int* ndim, const cubareal xx[], const int* ncomp, cubareal ff[], void* userdata);
+    int G_integrand(const int* ndim, const cubareal xx[], const int* ncomp, cubareal ff[], void* userdata);
 
-    double G_by_integration (double x1, double x2, double y1, double y2);
+    double G_by_integration(double x1, double x2, double y1, double y2);
 
-    double G_wrapper (double r, double rb, double theta);
+    double G_wrapper(double r, double rb, double theta);
 
-    double G (double x1, double x2, double y1, double y2);
+    double G(double x1, double x2, double y1, double y2);
 }
