d7947ade047a45aabea2044b789c71dd4bf27d65
Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>
2023-09-14 22:38:11 +0200

    Removed old matrix elements and started adding dilute approximation.

--- include/SaturationModel.hpp
+++ include/SaturationModel.hpp
@@ -1,55 +1,31 @@
 #pragma once
 
-namespace SaturationModel {
-    double D (double x);
+namespace SaturationModel
+{
+    double D(double G);
 
-    double DD (double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
+    double D_dilute(double G);
 
+    double DD(double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
 
-    double dsigma_d2b_sqr (double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
+    double DD_dilute(double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
 
-    double dsigma_d2b (double x1, double x2, double y1, double y2);
+    double dsigma_d2b_sqr(double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
 
-    /*namespace CorrelationMatrixElements {
+    double dsigma_d2b(double x1, double x2, double y1, double y2);
 
-        double G_xy (double b1, double b2, double r1, double r2, double bb1, double bb2, double rb1, double rb2);
+    namespace DDCorrelationMatrixElements
+    {
+        double T_xy_ybxb(double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
 
-        double G_xyb (double b1, double b2, double r1, double r2, double bb1, double bb2, double rb1, double rb2);
+        double T_xxb_yby(double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
 
-        double G_xby (double b1, double b2, double r1, double r2, double bb1, double bb2, double rb1, double rb2);
+        double a(double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
 
-        double G_xbyb (double b1, double b2, double r1, double r2, double bb1, double bb2, double rb1, double rb2);
+        double b(double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
 
-        double G_xxb (double b1, double b2, double r1, double r2, double bb1, double bb2, double rb1, double rb2);
+        double c(double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
 
-        double G_yyb (double b1, double b2, double r1, double r2, double bb1, double bb2, double rb1, double rb2);
-
-        double T1 (double b1, double b2, double r1, double r2, double bb1, double bb2, double rb1, double rb2);
-
-        double T2 (double b1, double b2, double r1, double r2, double bb1, double bb2, double rb1, double rb2);
-
-        double a (double b1, double b2, double r1, double r2, double bb1, double bb2, double rb1, double rb2);
-
-        double b (double b1, double b2, double r1, double r2, double bb1, double bb2, double rb1, double rb2);
-
-        double c (double b1, double b2, double r1, double r2, double bb1, double bb2, double rb1, double rb2);
-
-        double d (double b1, double b2, double r1, double r2, double bb1, double bb2, double rb1, double rb2);
-    }*/
-
-    namespace DDCorrelationMatrixElements {
-        double T_xy_ybxb (double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
-
-        double T_xxb_yby (double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
-
-        double a (double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
-
-        double b (double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
-
-        double c (double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
-
-        double d (double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
+        double d(double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
     }
-
-    double DDEigen (double x1, double x2, double y1, double y2, double xb1, double xb2, double yb1, double yb2);
 }
