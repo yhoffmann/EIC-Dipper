f2bdfbce4c1305711c56d6f737a460d41f5b64ef
Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>
2024-11-23 14:00:56 +0100

    Added more values to default g2mu02 vec.

--- src/Output.cpp
+++ src/Output.cpp
@@ -42,7 +42,7 @@ namespace Output
 
     std::vector<double> get_default_g2mu02_factor_vec()
     {
-        return std::vector<double>{0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5};
+        return std::vector<double>{0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5};
     }
 
     std::vector<double> get_default_phi_vec()
