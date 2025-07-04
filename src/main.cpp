c6304d28929a47032cc7e264f214aff3fd7b3934
Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>
2025-07-04 11:56:45 +0200

    chore(main):
    -(not important) run nucleus avg functions instead of cross section
    function

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,6 @@
 int main(int argc, char** argv) {
   init(argc, argv);
 
-  Output::dsigmadt_nucleus(1, 3, g_seed);
+  // Output::dsigmadt_nucleus(1, 3, g_seed);
+  Output::hotspot_nucleus_thickness_avg(A, H, g_seed, 16, g_filepath);
 }
